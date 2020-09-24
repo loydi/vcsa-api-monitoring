b31decf29214973338f97eeda13756dd4f5573de
Kemal YILDIRIM <32053602+loydi@users.noreply.github.com>
2020-09-24 14:40:33 +0200

    add send perf data

--- check_vcsa.py
+++ check_vcsa.py
@@ -6,7 +6,13 @@ import json
 import argparse
 import sys
 import os
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import urllib3
+import datetime
+date1 = datetime.datetime.utcnow() + datetime.timedelta(minutes=-15)
+date2 = datetime.datetime.utcnow()
+start_date = date1.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+end_date = date2.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+
 
 def get_arguments():
     parser = argparse.ArgumentParser()
@@ -32,7 +38,7 @@ def get_vcenter_datastore_output(args,json):
         w = False
         for dst in json['value']: 
             percent = (float(dst['free_space'])/float(dst['capacity'])*100)
-            print("Datastore name : {} / Datastore Free Space : {}  / Datastore Used : {:.2f}%" .format(dst['name'],pretty_size(float(dst['free_space'])),percent))
+            print("Datastore name : {} / Datastore Free Space : {}  / Datastore Used : {:.2f}% | '{} Free Space'={};;;;".format(dst['name'],pretty_size(float(dst['free_space'])),percent,dst['name'],dst['free_space']))
             if args.warning and args.ciritical:
                 if (percent >= int(args.warning)) and (percent < int(args.ciritical)):
                     sys.exit(1)
@@ -42,7 +48,7 @@ def get_vcenter_datastore_output(args,json):
         for dst in json['value']: 
             if args.filter in dst['name']:
                 percent = (float(dst['free_space'])/float(dst['capacity'])*100)
-                print("Datastore name : {} / Datastore Free Space : {}  / Datastore Used : {:.2f}%" .format(dst['name'],pretty_size(float(dst['free_space'])),percent))           
+                print("Datastore name : {} / Datastore Free Space : {}  / Datastore Used : {:.2f}% | '{} Free Space'={};;;;".format(dst['name'],pretty_size(float(dst['free_space'])),percent,dst['name'],dst['free_space']))         
                 
 def pretty_size(bytes):
     """
@@ -104,21 +110,37 @@ def main():
         if (not args.host) and (not args.host_username) and (not args.host_password):
             print("You must add credentials")
             sys.exit(2)
-    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     if args.type:
-        api_url ="https://{}/rest/appliance/health/{}".format(args.host,args.type)
+        if args.type == "storage":
+            api_url ="https://{}/rest/appliance/health/{}".format(args.host,args.type)
+            response = get_vcenter_health_status(api_url,args)
+            if response['value'] == "green":
+                print("OK : vCenter {} health : {}".format(args.type,response['value']))
+                sys.exit(0)
+            elif ((response['value'] == "orange") and (response['value'] == "yellow")):
+                print("WARNING : vCenter {} health : {}".format(args.type,response['value']))
+                sys.exit(1)
+            elif response['value'] == "red":
+                print("CRITICAL : vCenter {} health : {}".format(args.type,response['value']))
+                sys.exit(2)
+            else:
+                print("UNKOWN : vCenter {} health : {}".format(args.type,response['value']))
+                sys.exit(3)
+        api_url ="https://{}/rest/appliance/monitoring/query?item.names={}&item.interval=MINUTES30&item.function=AVG&item.start_time={}&item.end_time={}".format(args.host,args.type,start_date,end_date)
         response = get_vcenter_health_status(api_url,args)
-        if response['value'] == "green":
-            print("OK : vCenter {} health : {}".format(args.type,response['value']))
-            sys.exit(0)
-        elif ((response['value'] == "orange") and (response['value'] == "yellow")):
-            print("WARNING : vCenter {} health : {}".format(args.type,response['value']))
-            sys.exit(1)
-        elif response['value'] == "red":
-            print("CRITICAL : vCenter {} health : {}".format(args.type,response['value']))
+        percent_data = response['value'][0]['data'][0]
+        if percent_data > args.ciritical:
+            print("CRITICAL : vCenter {} is : {} | '{}'={};;;; ".format(args.type,percent_data,args.type,percent_data))
             sys.exit(2)
+        elif percent_data > args.warning:
+            print("WARNING : vCenter {} is : {} | '{}'={};;;; ".format(args.type,percent_data,args.type,percent_data))
+            sys.exit(1)
+        elif percent_data < args.warning:
+            print("OK : vCenter {} is : {} | '{}'={};;;; ".format(args.type,percent_data,args.type,percent_data))
+            sys.exit(0)
         else:
-            print("UNKOWN : vCenter {} health : {}".format(args.type,response['value']))
+            print("UNKOWN : vCenter {} ".format(args.type))
             sys.exit(3)
     if args.datastore:
         if args.datastore == "ALL":
